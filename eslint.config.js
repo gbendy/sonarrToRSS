6182d85e189e436950871f41dc3f66b86374cd19
Brendan Hack <github@bendys.com>
2024-03-09 22:02:03 +1100

    Linting of handlebars files.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const stylisticTs  = require('@stylistic/eslint-plugin-ts');
+const html = require('@html-eslint/eslint-plugin');
+const parser = require('@html-eslint/parser');
 
 module.exports = tseslint.config(
   eslint.configs.recommended,
@@ -14,6 +16,16 @@ module.exports = tseslint.config(
       '@/quotes': [ 'error', 'single' ],
       'camelcase': [ 'error' ],
       'no-trailing-spaces': [ 'error']
+    }
+  },
+  {
+    ...html.configs['flat/recommended'],
+    files: ['src/**/*.handlebars'],
+    rules: {
+      ...html.configs['flat/recommended'].rules,
+      '@html-eslint/indent': [ 'off', 2 ],
+      '@html-eslint/element-newline': [ 'off' ],
+      '@html-eslint/no-duplicate-id': [ 'off' ]
     }
   }
 );
